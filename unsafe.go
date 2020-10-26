b8a07ab97098ac164aafd018e85a9822f64d8c9a
Phil Kedy <phil.kedy@gmail.com>
2020-10-26 16:51:39 -0400

    Removing unused code

--- unsafe.go
+++ unsafe.go
@@ -7,18 +7,6 @@ import (
 	"unsafe"
 )
 
-// NOTE:
-// all of the definition in this file
-// should be repeated in appengine.go,
-// but without using unsafe
-
-const (
-	// spec says int and uint are always
-	// the same size, but that int/uint
-	// size may not be machine word size
-	smallint = unsafe.Sizeof(int(0)) == 4
-)
-
 // UnsafeString returns the byte slice as a volatile string
 // THIS SHOULD ONLY BE USED BY THE CODE GENERATOR.
 // THIS IS EVIL CODE.
