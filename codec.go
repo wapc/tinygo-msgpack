c4f5b67b65039c1276135d2b540d828b8ecd74b9
Phil Kedy <phil.kedy@gmail.com>
2020-10-30 14:44:50 -0400

    Adding ToBytes utility function

--- codec.go
+++ codec.go
@@ -6,3 +6,17 @@ type Codec interface {
 	Decode(decoder *Decoder) error
 	Encode(encoder Writer) error
 }
+
+// ToBytes creates a `[]byte` from `codec`.
+func ToBytes(codec Codec) ([]byte, error) {
+	var sizer Sizer
+	if err := codec.Encode(&sizer); err != nil {
+		return nil, err
+	}
+	buffer := make([]byte, sizer.Len())
+	encoder := NewEncoder(buffer)
+	if err := codec.Encode(&encoder); err != nil {
+		return nil, err
+	}
+	return buffer, nil
+}
